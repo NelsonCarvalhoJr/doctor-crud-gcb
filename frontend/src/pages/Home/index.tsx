f6d51903ab769edeb7acd904d9b1930f867ec4cb
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-02-04 21:28:49 -0300

    Added filters

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -12,6 +12,8 @@ import {
   DoctorsTableHead,
   DoctorsTableBody,
   DoctorsTableFoot,
+  FilterContainer,
+  FilterContent,
 } from './styles';
 
 interface IDoctor {
@@ -30,6 +32,11 @@ interface IDoctor {
 
 const Home: React.FC = () => {
   const [doctors, setDoctors] = useState<IDoctor[]>([] as IDoctor[]);
+  const [name, setName] = useState('');
+  const [crm, setCrm] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [cell_phone, setCellPhone] = useState('');
+  const [postcode, setPostcode] = useState('');
 
   useEffect(() => {
     api.get<IDoctor[]>(`/doctors`).then(response => {
@@ -49,11 +56,101 @@ const Home: React.FC = () => {
     setDoctors(updatedDoctors);
   }
 
+  function handleClearFields(): void {
+    setName('');
+    setCrm('');
+    setTelephone('');
+    setCellPhone('');
+    setPostcode('');
+  }
+
+  async function handleFilterData(): Promise<void> {
+    const filterData = {
+      name,
+      telephone,
+      cell_phone,
+      postcode,
+    };
+
+    if (crm) {
+      Object.assign(filterData, { crm });
+    }
+
+    const response = await api.get<IDoctor[]>(`/doctors`, {
+      params: filterData,
+    });
+    const doctorsData = response.data;
+
+    if (doctorsData) {
+      setDoctors(doctorsData);
+    } else {
+      setDoctors([] as IDoctor[]);
+    }
+  }
+
   return (
     <>
       <Header title="Home" />
       <HomeContainer>
         <HomeTitle>Listagem de Médicos</HomeTitle>
+
+        <FilterContainer>
+          <FilterContent>
+            <input
+              name="name"
+              type="text"
+              placeholder="Nome"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+          </FilterContent>
+          <FilterContent>
+            <input
+              name="crm"
+              type="text"
+              placeholder="CRM"
+              value={crm}
+              onChange={e => setCrm(e.target.value)}
+            />
+          </FilterContent>
+          <FilterContent>
+            <input
+              name="telephone"
+              type="text"
+              placeholder="Telefone"
+              value={telephone}
+              onChange={e => setTelephone(e.target.value)}
+            />
+          </FilterContent>
+          <FilterContent>
+            <input
+              name="cell_phone"
+              type="text"
+              placeholder="Celular"
+              value={cell_phone}
+              onChange={e => setCellPhone(e.target.value)}
+            />
+          </FilterContent>
+          <FilterContent>
+            <input
+              name="postcode"
+              type="text"
+              placeholder="CEP"
+              value={postcode}
+              onChange={e => setPostcode(e.target.value)}
+            />
+          </FilterContent>
+          <FilterContent>
+            <button type="button" onClick={handleFilterData}>
+              Filtrar
+            </button>
+          </FilterContent>
+          <FilterContent>
+            <button type="button" onClick={handleClearFields}>
+              Limpar
+            </button>
+          </FilterContent>
+        </FilterContainer>
 
         <DoctorsTable>
           <DoctorsTableHead>
@@ -70,7 +167,7 @@ const Home: React.FC = () => {
           </DoctorsTableHead>
 
           <DoctorsTableBody>
-            {doctors &&
+            {doctors.length ? (
               doctors.map(doctor => (
                 <tr key={doctor.id}>
                   <td>{doctor.id}</td>
@@ -92,7 +189,12 @@ const Home: React.FC = () => {
                     </a>
                   </td>
                 </tr>
-              ))}
+              ))
+            ) : (
+              <tr>
+                <td colSpan={8}>Nenhum dado encontrado</td>
+              </tr>
+            )}
           </DoctorsTableBody>
 
           <DoctorsTableFoot>
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const HomeContainer = styled.div`
   width: 100%;
@@ -42,5 +43,50 @@ export const DoctorsTableFoot = styled.tfoot`
   text-align: left;
   td {
     padding: 0 15px;
+  }
+`;
+
+export const FilterContainer = styled.div`
+  background: #333;
+  width: 100%;
+  padding: 5px 20px;
+  margin: 20px 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FilterContent = styled.div`
+  background: #333;
+  width: 100%;
+  padding: 10px 10px;
+
+  input {
+    width: 100%;
+    height: 30px;
+    border-radius: 10px;
+    padding: 5px;
+    background: #e8e8e8;
+    border: 0;
+    color: #333;
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  button {
+    background: #3333c3;
+    width: 100%;
+    height: 30px;
+    border-radius: 10px;
+    border: 0;
+    padding: 0 16px;
+    color: #e8e8e8;
+    font-weight: 500;
+    transition: background-color 0.2s;
+    &:hover {
+      background: ${shade(0.2, '#3333c3')};
+    }
   }
 `;
